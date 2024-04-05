e805bf0002c646ea4b001ded2bfcc60485edf1a4
prateek <prateeksingh8841@gmail.com>
2024-04-04 21:50:53 -0600

    feat: rn

---
+++ App.js
@@ -0,0 +1,21 @@
+import { StyleSheet, View, TextInput, Text, Button } from "react-native";
+
+export default function App() {
+  return (
+    <View style={styles.appContainer}>
+      <View>
+        <TextInput placeholder="Your course goal" />
+        <Button title="Add Goal" />
+      </View>
+      <View>
+        <Text>List of goals..</Text>
+      </View>
+    </View>
+  );
+}
+
+const styles = StyleSheet.create({
+  appContainer: {
+    padding: 50,
+  },
+});
