2cdc76eb4c77a11b2ec34c61f0cfa9def192c830
prateek <prateeksingh8841@gmail.com>
2024-04-10 20:14:03 -0600

    🧭  feat: navigation

--- App.js
+++ App.js
@@ -1,78 +1,12 @@
-import { useState } from "react";
-import { StyleSheet, View, FlatList, Button, StatusBar } from "react-native";
+// import { Component } from "react";
+import CategoriesScreen from "./screens/CategoriesScreen";
+// import { StyleSheet, Text, View } from "react-native";
+// import { TabNavigator } from "@react-navigation/native";
+// import AuthScreen from "./screens/AuthScreen";
+// import WelcomeScreen from "./screens/WelcomeScreen";
 
-import GoalItem from "./components/GoalItem";
-import GoalInput from "./components/GoalInput";
-
-export default function App() {
-  const [modalIsVisible, setModalIsVisible] = useState(false);
-  const [courseGoals, setCourseGoals] = useState([]);
-
-  function startAddGoalHandler() {
-    setModalIsVisible(true);
-  }
-
-  function endGoalHandler() {
-    setModalIsVisible(false);
-  }
-
-  function addGoalHandler(enteredText) {
-    setCourseGoals((currentCourseGoals) => [
-      ...currentCourseGoals,
-      { text: enteredText, id: Math.random().toString() },
-    ]);
-    endGoalHandler();
-  }
-
-  function deleteGoalHandler(id) {
-    setCourseGoals((currentCourseGoals) => {
-      return currentCourseGoals.filter((goal) => goal.id !== id);
-    });
-  }
-
-  return (
-    <>
-      <StatusBar />
-      <View style={styles.appContainer}>
-        <Button
-          title="Add New Goal"
-          color="#5e0acc"
-          onPress={startAddGoalHandler}
-        />
-
-        <GoalInput
-          visible={modalIsVisible}
-          onAddGoal={addGoalHandler}
-          onCancel={endGoalHandler}
-        />
-        <View style={styles.goalsContainer}>
-          <FlatList
-            data={courseGoals}
-            renderItem={(itemData) => {
-              return (
-                <GoalItem
-                  itemData={itemData}
-                  onDeleteItem={deleteGoalHandler}
-                />
-              );
-            }}
-            alwaysBounceVertical={false}
-            keyExtractor={(item, index) => item.id}
-          ></FlatList>
-        </View>
-      </View>
-    </>
-  );
+function App() {
+  return <CategoriesScreen />;
 }
 
-const styles = StyleSheet.create({
-  appContainer: {
-    paddingTop: 50,
-    paddingHorizontal: 16,
-    flex: 1,
-  },
-
-  goalsContainer: {
-    flex: 6,
-  },
-});
+export default App;
