03af834fadb8d2a12b0e0b0f25bf4fa46a2df4bb
prateek <prateeksingh8841@gmail.com>
2024-04-11 00:31:27 -0600

    🧭  feat: navigation

--- components/CategoryGridTile.js
+++ components/CategoryGridTile.js
@@ -1,8 +1,10 @@
 import { Pressable, View, Text } from "react-native";
+// import { useNavigation } from '@react-navigation/native';
 
 function CategoryGridTile({ title, color, onPress }) {
+  // const navigation = useNavigation();
   return (
-    <View style={{margin: 20}}>
+    <View style={{ margin: 20 }}>
       <Pressable onPress={onPress}>
         <View>
           <Text>{title}</Text>
--- screens/MealsOverviewScreen.js
+++ screens/MealsOverviewScreen.js
@@ -1,9 +1,25 @@
 import { MEALS } from "../data/dummyData";
-import { View, Text } from "react-native";
-function MealsOverviewScreen() {
+import { View, Text, FlatList } from "react-native";
+import MealItem from "../components/MealItem";
+
+function MealsOverviewScreen({ route }) {
+  const catId = route.params.categoryId; // get category id
+
+  const displayedMeals = MEALS.filter((item) => {
+    return item.categoryIds.indexOf(catId) >= 0; // gets the meals of this category
+  });
+
+  function renderMealItem(itemData) {
+    return <MealItem title={itemData.item.title} />;
+  }
+
   return (
     <View>
-      <Text>Meals Overview Screen</Text>
+      <FlatList
+        data={displayedMeals}
+        keyExtractor={(item) => item.id}
+        renderItem={renderMealItem}
+      />
     </View>
   );
 }
